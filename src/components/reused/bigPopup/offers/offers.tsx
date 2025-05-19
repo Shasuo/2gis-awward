9de65edfe237e9b5a8b0015aee959846c19a29e4
KS3N14 <kseniansk@mail.ru>
2025-05-19 19:41:04 +0700

    offers fixed

--- src/components/reused/bigPopup/offers/offers.tsx
+++ src/components/reused/bigPopup/offers/offers.tsx
@@ -8,31 +8,25 @@ export const Offers = () => {
     {
       header: "На аватарку в\xa0соцсетях",
       text: "Обновите фото профиля добавив шильдик, чтобы увидели клиенты.",
-      img: {
-        class: styles.image1,
-        src: `${IMAGES_PATH}/post-stories-with-shiels.png`
-      }
+      src: `${IMAGES_PATH}/post-stories-with-shiels.png`,
+      className: "normal"
     },
     {
       header: "На сайт",
       text: "Добавьте шильдик в раздел «О\xa0нас», на главную страницу или\xa0в карточки товаров/услуг, чтобы подчеркнуть доверие клиентов.",
-      img: {
-        class: styles.image,
-        src: `${IMAGES_PATH}/site-phone.png`
-      }
+      src: `${IMAGES_PATH}/site-phone.png`,
+      className: "order"
     },
     {
       header: "В посты и сторис",
       text: "Размещайте шильдик на картинках для соцсетей, когда рассказываете о премии или благодарите клиентов.",
-      img: {
-        src: `${IMAGES_PATH}/posts-stories-sale.png`
-      }
+      src: `${IMAGES_PATH}/posts-stories-sale.png`
     }
   ]
   return (
     <section className={styles.container}>
       <h3 className={styles.header}>Куда добавить диджитал-шильдик?</h3>
-      {/* <div className={styles.offers_conteiner}>
+      <div className={styles.offers_container}>
         { offers.map( (offer, index) =>
           (<div key={index} className={styles.item}>
           <div>
@@ -41,44 +35,13 @@ export const Offers = () => {
               {offer.text}
             </p>
           </div>
-          <img {offer.img.class && {className=offer.img.class}} src={offer.img.src} alt={""} />
+          <img 
+          className={offer.className ? offer.className === "normal" ? styles.image1 : styles.image : ""}
+          src={offer.src} 
+          alt={""} 
+          />
         </div>))
         }
-      </div> */}
-      <div className={styles.offers_conteiner}>
-        <div className={styles.item}>
-          <div>
-            <h4 className={styles.item_header}>На аватарку в&nbsp;соцсетях</h4>
-            <p className={styles.item_text}>
-              Обновите фото профиля добавив шильдик, чтобы увидели клиенты.
-            </p>
-          </div>
-          <img className={styles.image1} src={`${IMAGES_PATH}/post-stories-with-shiels.png`} alt={""} />
-        </div>
-        <div className={styles.item}>
-          <img
-            src={`${IMAGES_PATH}/site-phone.png`}
-            className={styles.image}
-            alt={""}
-          />
-          <div>
-            <h4 className={styles.item_header}>На сайт</h4>
-            <p className={styles.item_text}>
-              Добавьте шильдик в раздел «О&nbsp;нас», на главную страницу или&nbsp;в
-              карточки товаров/услуг, чтобы подчеркнуть доверие клиентов.
-            </p>
-          </div>
-        </div>
-        <div className={styles.item}>
-          <div>
-            <h4 className={styles.item_header}>В посты и сторис</h4>
-            <p className={styles.item_text}>
-              Размещайте шильдик на картинках для соцсетей, когда рассказываете
-              о премии или благодарите клиентов.
-            </p>
-          </div>
-          <img src={`${IMAGES_PATH}/posts-stories-sale.png`} alt={""} />
-        </div>
       </div>
       <div
         className={`hover:bg-[#666D7D] active:bg-[#666D7D] bg-[#374463b2] ${styles.back_button} cursor-pointer`}
