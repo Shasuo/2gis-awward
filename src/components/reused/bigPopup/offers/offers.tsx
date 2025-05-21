5aa8efee5fce3eda7521f9cd2af1e21f29dc74c2
K53N14 <you@example.com>
2025-05-21 14:34:11 +0700

    kz styling

--- src/components/reused/bigPopup/offers/offers.tsx
+++ src/components/reused/bigPopup/offers/offers.tsx
@@ -13,27 +13,27 @@ export const Offers = () => {
       className: "normal"
     },
     {
-      header: `${language ? "На сайт" : "Сайтқа"}`,
-      text: `${language ? "Добавьте шильдик в раздел «О\xa0нас», на главную страницу или\xa0в карточки товаров/услуг, чтобы подчеркнуть доверие клиентов." : "Клиенттердің сеніміне ерекше көңіл аудару үшін шильдті «Біз туралы» бөліміне, басты бетке немесе тауарлар/қызметтер карточкаларына қосыңыз."}`,
-      src: `${IMAGES_PATH}/site-phone.png`,
-      className: "order"
-    },
-    {
       header: `${language ? "В посты и сторис" : "Посттар мен стористерге"}`,
       text: `${language ? "Размещайте шильдик на картинках для соцсетей, когда рассказываете о премии или благодарите клиентов." : "Шильдті 2GIS Awards туралы ақпаратпен бөліскенде немесе клиенттерге алғыс айтқан кезде әлеуметтік желілерге арналған суреттерге орналастырыңыз."}`,
       src: `${IMAGES_PATH}/posts-stories-sale.png`,
+      className: "order",
       itemHeight: "kz"
-    }
+    },
+    {
+      header: `${language ? "На сайт" : "Сайтқа"}`,
+      text: `${language ? "Добавьте шильдик в раздел «О\xa0нас», на главную страницу или\xa0в\xa0карточки товаров/услуг, чтобы подчеркнуть доверие клиентов." : "Клиенттердің сеніміне ерекше көңіл аудару үшін шильдті «Біз туралы» бөліміне, басты бетке немесе тауарлар/қызметтер карточкаларына қосыңыз."}`,
+      src: `${IMAGES_PATH}/site-phone.png`
+    } 
   ]
   return (
     <section className={styles.container}>
-      <h3 className={styles.header}>{language ? "Куда добавить диджитал-шильдик?" : "Диджитал-шильдті қайда қосуға болады?"}</h3>
+      <h3 className={language ? styles.header : styles.headerKZ + ' ' + styles.header}>{language ? "Куда добавить диджитал-шильдик?" : "Диджитал-шильдті қайда қосуға болады?"}</h3>
       <div className={styles.offers_container}>
         { offers.map( (offer, index) =>
           (<div key={index} className={!language && offer.itemHeight ? styles.item2 + ' ' + styles.item : styles.item}>
           <div>
             <h4 className={language ? styles.item_header : styles.item_headerKZ + ' ' + styles.item_header}>{offer.header}</h4>
-            <p className={styles.item_text}>
+            <p className={language ? styles.item_text : styles.item_textKZ + ' ' + styles.item_text}>
               {offer.text}
             </p>
           </div>
