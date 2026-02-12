b877a83e053e719f67e9a70bf236a47bffdf053a
KS3N14 <kseniansk@mail.ru>
2026-02-12 19:07:45 +0700

    added mts tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,19 @@ export default function Document() {
               })(window,document,'script','dataLayer','GTM-MKSDVTS5');`,
           }}
         />
+        {/* MTS Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function (dl, id) {
+              window[dl] = window[dl] || [];
+              const script = document.createElement('script'),
+                    params = dl !== 'dataLayer' ? '?l=' + dl : '';
+              script.async = true;
+              script.src = \`https://tag.a.mts.ru/container/\${id}/tag.js\${params}\`;
+              document.head.appendChild(script);
+            })('dataLayer', '019c5128-597e-7a91-a9a8-8a1052b0a67b');`,
+          }}
+        />
       </Head>
       <body className="antialiased">
         <noscript>
